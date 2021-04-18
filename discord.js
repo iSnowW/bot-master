534a3ffc8c79dfc1d7e14e7288caebd2fc36a2ab
Snow <68227260+iSnowW@users.noreply.github.com>
2021-04-18 19:56:09 -0300

    bot.js

---
+++ discord.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT); // Recebe solicitações que o deixa online
+
+const Discord = require("discord.js"); //Conexão com a livraria Discord.js
+const client = new Discord.Client(); //Criação de um novo Client
+const config = require("./config.json"); //Pegando o prefixo do bot para respostas de comandos
+
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (message.channel.type == 'dm') return;
+    if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return;
+    if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
+
+   const args = message.content
+       .trim().slice(config.prefix.length) 
+       .split(/ +/g);
+   const command = args.shift().toLowerCase();
+
+   try {
+       const commandFile = require(`./commands/${command}.js`)
+       commandFile.run(client, message, args);
+   } catch (err) {
+   console.error('Erro:' + err);
+ }
+});
+
+client.on("ready", () => {
+  let activities = [
+      `Utilize !help | para obter ajuda 💙.`,
+    ],
+    i = 0;
+  setInterval( () => client.user.setActivity(`${activities[i++ % activities.length]}`, {
+        type: "PLAYING"
+      }), 1000 * 60); 
+  client.user
+      .setStatus("on")
+      .catch(console.error); 
+console.log("Estou Online!")
+});
+
+
+
+client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
